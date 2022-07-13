0096f39690d6efdd53eef892a1e5ab9511748d79
nitin7340 <108212936+nitin7340@users.noreply.github.com>
2022-07-13 11:49:17 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   
+    char w='f';
+    if(97<='f' && 'f'<=122){
+        printf("LOWERCASE");
+    }
+    else{
+        printf("UPPERCASE");
+    }
+    return 0;
+}
